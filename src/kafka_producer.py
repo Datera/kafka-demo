c89664fe464962daf6d7cb491e406a86d62d27e5
Matt Smith <mss@datera.io>
2018-05-09 13:58:07 -0500

    Initial kafka producer script and requirements.txt

---
+++ src/kafka_producer.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from __future__ import (print_function, unicode_literals, division,
+                        absolute_import)
+
+import argparse
+import random
+import string
+import sys
+import threading
+import time
+import itertools
+
+import kafka
+import simplejson as json
+
+
+def gen_messages():
+    alc = list(string.ascii_lowercase)
+    while True:
+        yield "".join(alc[int(random.random() * 26)] for _ in range(100))
+
+
+def sender(producer):
+    messages = gen_messages(args.messages)
+    topic = args.topic
+    while True:
+        prod = random.choice(("producera", "producerb"))
+        dest = random.choice(("consumera", "consumerb"))
+        for msg in itertools.islice(messages, random.randint(100, 2000)):
+            producer.send(topic, {"log": msg, "dest": dest, "prod": prod})
+    print("All Done")
+
+
+def main(args):
+    producer = kafka.KafkaProducer(
+        bootstrap_servers=[args.broker],
+        value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+    send = threading.Thread(target=sender, args=(producer,))
+    send.daemon = True
+    send.start()
+    while True:
+        print(int(producer.metrics()['producer-metrics'][
+            'outgoing-byte-rate']), end='\r')
+        # print("-------")
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("topic")
+    parser.add_argument("broker")
+    parser.add_argument("-m", "--messages", default=1000000)
+    args = parser.parse_args()
+    sys.exit(main(args))
